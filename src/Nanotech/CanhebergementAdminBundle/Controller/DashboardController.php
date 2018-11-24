117f29a13f8c19ec95945ebcd3cb5e3268850c67
harold <wtakoutsing@gmail.com>
2018-11-24 19:03:50 +0100

    exemple de dashboard controller

--- src/Nanotech/CanhebergementAdminBundle/Controller/DashboardController.php
+++ src/Nanotech/CanhebergementAdminBundle/Controller/DashboardController.php
@@ -11,9 +11,14 @@ class DashboardController extends Controller
     //tu ecris les fonctions qui permettent de compter et tu utilise response. puis tu vas dans ressource core et tu vois commment j'appelle le controlleur si
     public function indexAction()
     {
-       
-        
-        return new Response(23);
+
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('NanotechCanhebergementBundle:Utilisateur')->findAll();
+        if (empty($entity)) {
+            return new Response(0);
+        }
+
+        return new Response(count($entity));
     }
     
       
